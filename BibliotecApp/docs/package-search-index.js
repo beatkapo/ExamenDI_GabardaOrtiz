2ca50c3bf913193d5da08f61c87089ed19ac6a28
Fran Gabarda <frangabardaortiz@gmail.com>
2024-02-28 17:34:45 +0100

    Finished

---
+++ BibliotecApp/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"es.beatkapo.bibliotecapp","l":"es.beatkapo.bibliotecapp"},{"m":"es.beatkapo.bibliotecapp","l":"es.beatkapo.bibliotecapp.model"}];updateSearchResults();
